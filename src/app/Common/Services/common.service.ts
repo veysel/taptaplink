425e0c468f931b72f0da3b6188305f2ea3ca11cb
veysel <vinanc72@gmail.com>
2018-11-06 23:19:28 +0300

    Api url added to service

--- src/app/Common/Services/common.service.ts
+++ src/app/Common/Services/common.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class CommonService {
 
     public GetServiceRootUrl(): string {
-        return "";
+        return "https://api.myjson.com/bins/wzae6";
     }
 
 }
